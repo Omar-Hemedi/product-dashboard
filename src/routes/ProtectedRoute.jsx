ed83f8d8ee59fd6e292bbb331be6108b507b2910
Omar Hemedi <omarhemedijumaa@gmail.com>
2025-04-06 19:28:29 +0300

    feat: implement ProtectedRoute component for authentication
    - Added ProtectedRoute component to protect routes based on authentication status.
    - Utilized Firebase's onAuthStateChanged to track authentication state.
    - Redirected unauthenticated users to the sign-in page.

---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../firebase";
+
+export default function ProtectedRoute({ children }) {
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setIsAuthenticated(!!user);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    if (isAuthenticated === null) {
+        return <div>Loading...</div>;
+    }
+
+    return isAuthenticated ? children : <Navigate to="/auth/sign-in" />;
+}
