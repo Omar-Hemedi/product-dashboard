994f0d0b0d7f1e47210587a08c5c68e80f5e0fd5
Omar Hemedi <omarhemedijumaa@gmail.com>
2025-04-06 19:21:54 +0300

    Added RouterProvider to wrap routes in App component for routing setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import { RouterProvider } from 'react-router-dom';
+import router from './routes/Router';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1 className="text-red-400">
-        Hello world!
-      </h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
